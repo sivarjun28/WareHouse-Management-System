8c565b56f8fcb70d653491769dfac81b1ade4be7
sivarjun28 <arjunyadav6407@gmail.com>
2024-06-20 18:48:57 +0530

    updated

---
+++ src/main/java/com/jsp/warehouse_management_system/entity/Batch.java
@@ -0,0 +1,27 @@
+package com.jsp.warehouse_management_system.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Batch {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int batchId;
+	int quantity;
+	
+	@ManyToOne
+	private Inventory inventory;
+}
---
+++ src/main/java/com/jsp/warehouse_management_system/exception/BatchNotFoundException.java
@@ -0,0 +1,5 @@
+package com.jsp.warehouse_management_system.exception;
+
+public class BatchNotFoundException {
+
+}
